32dfe479dd4ae6830a91af5f86ff22423d4188ca
lhnexus <liuhongdashi@hotmail.com>
2017-09-27 15:44:35 -0400

    update auto refresh

--- demo_ui/resources/Canvas/Track/work_orders.controller.js
+++ demo_ui/resources/Canvas/Track/work_orders.controller.js
@@ -4,18 +4,44 @@ sap.ui.define(["sap/ui/core/mvc/Controller", "sap/m/MessageToast", "../Util/svgU
 
 		return Controller.extend("Canvas.Track.work_orders", {
 			onInit: function() {
-
-				var oModel = new sap.ui.model.json.JSONModel();
-				//var oRModel = new sap.ui.model.odata.v2.ODataModel("/iotmms/v1/api/http/app.svc");
-				oModel.loadData("Canvas/mockserver/data.json");
+				// var oRModel = new sap.ui.model.odata.v2.ODataModel("/serviceorder.xsodata/serviceorderVehicleInfo?$format=json");
 			
-				this.getView().setModel(oModel, "jdata");
-				//this.getView().setModel(oRModel, "odata");
+				var jmodel = new sap.ui.model.json.JSONModel("/serviceorder.xsodata/serviceorderVehicleInfo?$format=json");
+				
+				this.getView().setModel(jmodel, "orderData");
+				//this.getView().byId("wo_table").setModel(oRModel,"orderData");
+				// var oModel = new sap.ui.model.json.JSONModel();
+				// //var oRModel = new sap.ui.model.odata.v2.ODataModel("/iotmms/v1/api/http/app.svc");
+				// oModel.loadData("Canvas/mockserver/data.json");
+			
+				// this.getView().setModel(oModel, "jdata");
+				// //this.getView().setModel(oRModel, "odata");
 
 			},
 
 			onAfterRendering: function() {
-
+				
+				var oRModel = new sap.ui.model.odata.v2.ODataModel("/serviceorder.xsodata", true);
+				// var wotable = this.getView().byId("wo_table");
+				var lmodel = this.getView().getModel("orderData");
+				setInterval(function() {
+					// lmodel.refresh(true);
+					// wotable.getBinding("rows").refresh(true);
+					oRModel.read("/serviceorderVehicleInfo", {
+						// filters: filter,
+						// sorters: sorter,
+						async: false,
+						success: function(oData, response) {
+						//var ljmodel = new sap.ui.model.json.JSONModel(oData);
+							lmodel.oData.d.setData(oData);
+						},
+						failed: function(oData, response) {
+								alert("Failed to get InputHelpValues from service!");
+							}
+							//console.log(oMetadata);
+					}, null);
+					
+				}, 2000);
 			}
 
 		});
